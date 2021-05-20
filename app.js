2bd4559ec256ba8c44ca8e1c5051203eda6ea4c1
Mohammad MuaZam <mm9084@srmist.edu.in>
2021-05-20 11:53:43 +0530

    Deploy 1.1

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ app.use(express.static(path.join(__dirname, "./public")));
 app.set("view engine", "ejs");
 
 app.use("/", routes);
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on https://localhost:${port}`);
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`Server is running on https://localhost:${port}`);
+// });
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
